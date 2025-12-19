c40543d41295ca30995fbfe96c7bd24ef1bf7811
Grand-Master-Baiter <iv018264@gmail.com>
2025-12-19 21:12:38 +0300

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,29 +5,18 @@ from alembic import context
 import os
 import sys
 
-# Добавляем путь к приложению
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from app.database import Base
 from app.models import User, Order, Delivery
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def get_url():
